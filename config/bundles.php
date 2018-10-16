eabce38b0af82d9955f9ed2d81c74f5fb0dfd998
Jimmy Zhao <itomgit@gmail.com>
2018-10-16 16:35:33 +0800

    Require more bundles

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,7 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
 ];
