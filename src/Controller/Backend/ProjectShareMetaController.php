cfce7ce041539b8ac2f26d33779ebd075918ff8b
yadi <yadi>
2019-02-13 22:49:50 -0500

    fix backend banner update

--- src/Controller/Backend/ProjectShareMetaController.php
+++ src/Controller/Backend/ProjectShareMetaController.php
@@ -3,12 +3,16 @@
 namespace App\Controller\Backend;
 
 use App\Entity\ProjectShareMeta;
+use App\Entity\ShareSource;
 use App\Form\ProjectShareMetaType;
 use App\Repository\FileRepository;
 use App\Repository\ProjectShareMetaRepository;
+use App\Repository\ShareSourceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\File;
 
 /**
  * @Route("/backend")
@@ -40,6 +44,7 @@ class ProjectShareMetaController extends BackendController
         $form = $this->createForm(ProjectShareMetaType::class, $projectShareMetum);
 
         // init shareBannerFileId
+        $shareSources = [];
         if ($projectShareMetum->isBannerEditable() && $projectShareMetum->getShareBannerFileId()) {
             $shareBannerFileId = [];
             $file = $fileRepository->find($projectShareMetum->getShareBannerFileId());
@@ -50,14 +55,36 @@ class ProjectShareMetaController extends BackendController
                 'size' => $file->getSize()
             ];
             $form->get('shareBannerFileId')->setData($shareBannerFileId);
+
+            $shareSourceRepository = $this->getEntityManager()->getRepository(ShareSource::class);
+            /**
+             * @var ShareSource[] $shareSources
+             */
+            $shareSources = $shareSourceRepository->findBy(['bannerFile' => $projectShareMetum->getShareBannerFileId()]);
+
+            foreach($shareSources as $shareSource) {
+                $shareSources[] = $shareSource;
+            }
         }
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             if ($projectShareMetum->isBannerEditable()) {
+                $fileRepository = $this->getEntityManager()->getRepository(File::class);
                 $shareBannerFileId = isset($request->request->get('project_share_meta')['shareBannerFileId']) ? $request->request->get('project_share_meta')['shareBannerFileId'] : null;
                 $projectShareMetum->setShareBannerFileId($shareBannerFileId);
+
+                //更新相关的share source
+                foreach ($shareSources as $shareSource) {
+                    /**
+                     * @var File $file
+                     */
+                    $file = $fileRepository->find($shareBannerFileId);
+                    $shareSource->setBannerFile($file);
+                    $this->getEntityManager()->persist($shareSource);
+                }
+
             }
 
             $this->getDoctrine()->getManager()->flush();
