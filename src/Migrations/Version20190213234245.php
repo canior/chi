3070873de17c12defd6c0ea771fe8d8958df445b
yadi <yadi>
2019-02-13 19:17:00 -0500

    add user log

--- src/Migrations/Version20190213234245.php
+++ src/Migrations/Version20190213234245.php
@@ -20,7 +20,7 @@ final class Version20190213234245 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user_log (id INT AUTO_INCREMENT NOT NULL, type varchar(255) not null, user_id INT NOT NULL, type varchar(50) not null, log text COLLATE utf8mb4_unicode_ci, backtrace text, created_at INT NOT NULL, PRIMARY KEY(id)) ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_log (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, type varchar(50) not null, log text COLLATE utf8mb4_unicode_ci, backtrace text, created_at INT NOT NULL, PRIMARY KEY(id)) ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
