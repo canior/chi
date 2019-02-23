f10a5b8699e3e0f5f188db593d43b2ac9c94c014
yadi <yadi>
2019-02-23 15:02:09 +0800

    fix id number

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -112,6 +112,11 @@ class UserController extends BaseController
                     $user->setAvatarUrl($avatarUrl);
                     $user->info("update user nickname to " . $nickName . " and avatar url");
                 }
+
+                if ($user->getIdNum() == null) {
+                    $user->setIdNum(""); //临时修改身份证号码
+                }
+
                 $user->setLastLoginTimestamp(time());
 
                 $this->getEntityManager()->persist($user);
@@ -147,6 +152,11 @@ class UserController extends BaseController
                     $user->setAvatarUrl($avatarUrl);
                     $user->info("update user nickname to " . $nickName . " and avatar url");
                 }
+
+                if ($user->getIdNum() == null) {
+                    $user->setIdNum(""); //临时修改身份证号码
+                }
+
 
                 $this->getEntityManager()->persist($user);
                 $this->getEntityManager()->flush();
