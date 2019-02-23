110eb15d4347abf56c8bb3301d606c6843ecfb97
yadi <yadi>
2019-02-24 01:20:20 +0800

    update backend user level changes

--- src/Form/UserPersonalType.php
+++ src/Form/UserPersonalType.php
@@ -21,9 +21,6 @@ class UserPersonalType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $userLevelTexts = UserLevel::$userLevelTextArray;
-        unset($userLevelTexts[UserLevel::PARTNER]);
-
         $builder
             ->add('userLevel', ChoiceType::class, [
                 'label' => '会员等级',
