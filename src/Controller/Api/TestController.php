10b92dcf05a8577c0d8bda8f1a20d5fbb2b44e3f
yadi <yadi>
2019-02-25 02:55:50 +0800

    update video

--- src/Controller/Api/TestController.php
+++ src/Controller/Api/TestController.php
@@ -154,35 +154,8 @@ class TestController extends BaseController
      * @return Response
      */
     public function steamAction(Request $request) {
-//        $file = "/Users/tandy/Downloads/xuchuan1.mp4";
-//        $size = filesize($file);
-//        header("Content-type: video/mp4");
-//        header("Accept-Ranges: bytes");
-//        if(isset($_SERVER['HTTP_RANGE'])){
-//            header("HTTP/1.1 206 Partial Content");
-//            list($name, $range) = explode("=", $_SERVER['HTTP_RANGE']);
-//            list($begin, $end) =explode("-", $range);
-//            if($end == 0){
-//                $end = $size - 1;
-//            }
-//        }else {
-//            $begin = 0; $end = $size - 1;
-//        }
-//        header("Content-Length: " . ($end - $begin + 1));
-//        header("Content-Disposition: filename=".basename($file));
-//        header("Content-Range: bytes ".$begin."-".$end."/".$size);
-//        $fp = fopen($file, 'rb');
-//        fseek($fp, $begin);
-//        while(!feof($fp)) {
-//            $p = min(1024, $end - $begin + 1);
-//            $begin += $p;
-//            echo fread($fp, $p);
-//        }
-//        fclose($fp);
 
-
-
-        $url = "https://outin-a7944acc383b11e9a86700163e1a625e.oss-cn-shanghai.aliyuncs.com/1eb988a32edb433ebfc49fdec49cf984/7de1b77d1de64334af0bc0de92a15e4f-1aa2d4ce70716759229a940427edbc78-ld.m3u8?Expires=1551035506&OSSAccessKeyId=LTAI8bKSZ6dKjf44&Signature=r7s6O4X8QeLXAwh9XYBDFzel1OY%3D";
+        $url = "https://outin-a7944acc383b11e9a86700163e1a625e.oss-cn-shanghai.aliyuncs.com/1eb988a32edb433ebfc49fdec49cf984/7de1b77d1de64334af0bc0de92a15e4f-36f439e53c29dae4a99a5b4a441cab15-ld.mp4?Expires=1551035506&OSSAccessKeyId=LTAI8bKSZ6dKjf44&Signature=ryHfgwNohaqWAPuAkMJ9fo%2FVSzs%3D";
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
@@ -191,22 +164,118 @@ class TestController extends BaseController
         curl_setopt($curl, CURLOPT_TIMEOUT, 30);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
 
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        header("Content-Description: File Transfer");
-        header("Content-Transfer-Encoding: binary");
-        Header("Content-type: ". 'video/m3u8');
-        //Header("Content-Length: ".$fileSize);
-        flush();
+        $size = 255640000;
+        $length = $size;
+        $start = 0;
+        $end = $size - 1;
+        header('Content-type: video/mp4');
+        header("Accept-Ranges: 0-$length");
+        if (isset($_SERVER['HTTP_RANGE'])) {
+            $c_start = $start;
+            $c_end = $end;
+            list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+
+            if (strpos($range, ',') !== false) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $start-$end/$size");
+                exit;
+            }
+
+            if ($range == '-') {
+                $c_start = $size - substr($range, 1);
+            } else {
+                $range = explode('-', $range);
+                $c_start = $range[0];
+                $c_end = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+            }
+
+            $c_end = ($c_end > $end) ? $end : $c_end;
+
+            if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $start-$end/$size");
+                exit;
+            }
+
+            $start = $c_start;
+            $end = $c_end;
+            $length = $end - $start + 1;
+            header('HTTP/1.1 206 Partial Content');
+        }
+
+        header("Content-Range: bytes $start-$end/$size");
+        header("Content-Length: ".$length);
 
         $result = curl_exec($curl);
+
         curl_close($curl);
+    }
 
+    /**
+     * @Route("/testLocalStream", name="testLocalStream", methods="GET")
+     * @param Request $request
+     * @return Response
+     */
+    public function testLocalStream() {
+        $filepath = "/Users/tandy/Downloads/coming_soon.mp4";
+        // Determine file mimetype
+        $fp = fopen($filepath, "rb");
+        $size = filesize($filepath);
+        $length = $size;
+        $start = 0;
+        $end = $size - 1;
+        header('Content-type: video/mp4');
+        header("Accept-Ranges: 0-$length");
+        if (isset($_SERVER['HTTP_RANGE'])) {
+            $c_start = $start;
+            $c_end = $end;
+            list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
 
-//        header("Content-Type: video/mp4");
-//        echo file_get_contents("https://outin-a7944acc383b11e9a86700163e1a625e.oss-cn-shanghai.aliyuncs.com/sv/bcff41c-169202774dc/bcff41c-169202774dc.mp4?Expires=1551027219&OSSAccessKeyId=LTAI8bKSZ6dKjf44&Signature=FwJCcCUhggulPxYoAjXtwdjvbEI%3D");
-//        exit;
+            if (strpos($range, ',') !== false) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $start-$end/$size");
+                exit;
+            }
+
+            if ($range == '-') {
+                $c_start = $size - substr($range, 1);
+            } else {
+                $range = explode('-', $range);
+                $c_start = $range[0];
+                $c_end = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+            }
+
+            $c_end = ($c_end > $end) ? $end : $c_end;
+
+            if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $start-$end/$size");
+                exit;
+            }
+
+            $start = $c_start;
+            $end = $c_end;
+            $length = $end - $start + 1;
+            fseek($fp, $start);
+            header('HTTP/1.1 206 Partial Content');
+        }
+
+        header("Content-Range: bytes $start-$end/$size");
+        header("Content-Length: ".$length);
+
+        $buffer = 1024 * 8;
+
+        while(!feof($fp) && ($p = ftell($fp)) <= $end) {
+            if ($p + $buffer > $end) {
+                $buffer = $end - $p + 1;
+            }
+            set_time_limit(0);
+            echo fread($fp, $buffer);
+            flush();
+        }
+
+        fclose($fp);
+        exit;
     }
 
 }
--- src/Controller/UploadFileController.php
+++ src/Controller/UploadFileController.php
@@ -83,50 +83,65 @@ class UploadFileController extends DefaultController
             $response = new BinaryFileResponse($file->getAbsolutePath());
             return $response;
         } else if ($file->isVideo()) {
-//            $response = new BinaryFileResponse($file->getAbsolutePath());
-//            $response->setAutoEtag();
-//            $response->headers->set('Content-Type', 'video/ogg');
-//            // cache video for one week, not work for streaming?
-//            $response->setSharedMaxAge(604800);
+            $filepath = $file->getAbsolutePath();
+            // Determine file mimetype
+            $fp = fopen($filepath, "rb");
+            $size = filesize($filepath);
+            $length = $size;
+            $start = 0;
+            $end = $size - 1;
+            header('Content-type: video/mp4');
+            header("Accept-Ranges: 0-$length");
+            if (isset($_SERVER['HTTP_RANGE'])) {
+                $c_start = $start;
+                $c_end = $end;
+                list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
 
-//            $file = $file->getAbsolutePath();
-//            $size = filesize($file);
-//            header("Content-type: video/mp4");
-//            header("Accept-Ranges: bytes");
-//            if(isset($_SERVER['HTTP_RANGE'])){
-//                header("HTTP/1.1 206 Partial Content");
-//                list($name, $range) = explode("=", $_SERVER['HTTP_RANGE']);
-//                list($begin, $end) =explode("-", $range);
-//                if($end == 0){
-//                    $end = $size - 1;
-//                }
-//            }else {
-//                $begin = 0; $end = $size - 1;
-//            }
-//            header("Content-Length: " . ($end - $begin + 1));
-//            header("Content-Disposition: filename=".basename($file));
-//            header("Content-Range: bytes ".$begin."-".$end."/".$size);
-//            $fp = fopen($file, 'rb');
-//            fseek($fp, $begin);
-//            while(!feof($fp)) {
-//                $p = min(1024, $end - $begin + 1);
-//                $begin += $p;
-//                echo fread($fp, $p);
-//            }
-//            fclose($fp);
+                if (strpos($range, ',') !== false) {
+                    header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                    header("Content-Range: bytes $start-$end/$size");
+                    exit;
+                }
 
-            $url = "https://outin-a7944acc383b11e9a86700163e1a625e.oss-cn-shanghai.aliyuncs.com/1eb988a32edb433ebfc49fdec49cf984/7de1b77d1de64334af0bc0de92a15e4f-36f439e53c29dae4a99a5b4a441cab15-ld.mp4?Expires=1551035506&OSSAccessKeyId=LTAI8bKSZ6dKjf44&Signature=ryHfgwNohaqWAPuAkMJ9fo%2FVSzs%3D";
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE); // 从证书中检查SSL加密算法是否存在
-            curl_setopt($curl, CURLOPT_HEADER, 0);
-            curl_setopt($curl, CURLOPT_TIMEOUT, 30);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+                if ($range == '-') {
+                    $c_start = $size - substr($range, 1);
+                } else {
+                    $range = explode('-', $range);
+                    $c_start = $range[0];
+                    $c_end = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+                }
 
-            Header("Content-type: ". 'video/mp4');
-            $result = curl_exec($curl);
-            curl_close($curl);
+                $c_end = ($c_end > $end) ? $end : $c_end;
+
+                if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+                    header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                    header("Content-Range: bytes $start-$end/$size");
+                    exit;
+                }
+
+                $start = $c_start;
+                $end = $c_end;
+                $length = $end - $start + 1;
+                fseek($fp, $start);
+                header('HTTP/1.1 206 Partial Content');
+            }
+
+            header("Content-Range: bytes $start-$end/$size");
+            header("Content-Length: ".$length);
+
+            $buffer = 1024 * 8;
+
+            while(!feof($fp) && ($p = ftell($fp)) <= $end) {
+                if ($p + $buffer > $end) {
+                    $buffer = $end - $p + 1;
+                }
+                set_time_limit(0);
+                echo fread($fp, $buffer);
+                flush();
+            }
+
+            fclose($fp);
+            exit;
 
         }
     }
