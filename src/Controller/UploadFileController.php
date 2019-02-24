033400baf573f255455ea118a67657bb005ac93e
yadi <yadi>
2019-02-25 01:26:30 +0800

    tests video

--- src/Controller/UploadFileController.php
+++ src/Controller/UploadFileController.php
@@ -79,6 +79,7 @@ class UploadFileController extends DefaultController
         $file = $this->getDataAccess()->getDao(File::class, $fileId);
         if ($file->isImage()) {
             $response = new BinaryFileResponse($file->getAbsolutePath());
+            return $response;
         } else if ($file->isVideo()) {
 //            $response = new BinaryFileResponse($file->getAbsolutePath());
 //            $response->setAutoEtag();
@@ -86,34 +87,54 @@ class UploadFileController extends DefaultController
 //            // cache video for one week, not work for streaming?
 //            $response->setSharedMaxAge(604800);
 
-            $file = $file->getAbsolutePath();
-            $size = filesize($file);
-            header("Content-type: video/mp4");
-            header("Accept-Ranges: bytes");
-            if(isset($_SERVER['HTTP_RANGE'])){
-                header("HTTP/1.1 206 Partial Content");
-                list($name, $range) = explode("=", $_SERVER['HTTP_RANGE']);
-                list($begin, $end) =explode("-", $range);
-                if($end == 0){
-                    $end = $size - 1;
-                }
-            }else {
-                $begin = 0; $end = $size - 1;
-            }
-            header("Content-Length: " . ($end - $begin + 1));
-            header("Content-Disposition: filename=".basename($file));
-            header("Content-Range: bytes ".$begin."-".$end."/".$size);
-            $fp = fopen($file, 'rb');
-            fseek($fp, $begin);
-            while(!feof($fp)) {
-                $p = min(1024, $end - $begin + 1);
-                $begin += $p;
-                echo fread($fp, $p);
-            }
-            fclose($fp);
-        }
+//            $file = $file->getAbsolutePath();
+//            $size = filesize($file);
+//            header("Content-type: video/mp4");
+//            header("Accept-Ranges: bytes");
+//            if(isset($_SERVER['HTTP_RANGE'])){
+//                header("HTTP/1.1 206 Partial Content");
+//                list($name, $range) = explode("=", $_SERVER['HTTP_RANGE']);
+//                list($begin, $end) =explode("-", $range);
+//                if($end == 0){
+//                    $end = $size - 1;
+//                }
+//            }else {
+//                $begin = 0; $end = $size - 1;
+//            }
+//            header("Content-Length: " . ($end - $begin + 1));
+//            header("Content-Disposition: filename=".basename($file));
+//            header("Content-Range: bytes ".$begin."-".$end."/".$size);
+//            $fp = fopen($file, 'rb');
+//            fseek($fp, $begin);
+//            while(!feof($fp)) {
+//                $p = min(1024, $end - $begin + 1);
+//                $begin += $p;
+//                echo fread($fp, $p);
+//            }
+//            fclose($fp);
 
-        return $response;
+            $url = "https://outin-a7944acc383b11e9a86700163e1a625e.oss-cn-shanghai.aliyuncs.com/1eb988a32edb433ebfc49fdec49cf984/7de1b77d1de64334af0bc0de92a15e4f-36f439e53c29dae4a99a5b4a441cab15-ld.mp4?Expires=1551031951&OSSAccessKeyId=LTAI8bKSZ6dKjf44&Signature=AB4X%2FdCreC9pt99ihQR634UWSQE%3D";
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE); // 从证书中检查SSL加密算法是否存在
+            curl_setopt($curl, CURLOPT_HEADER, 0);
+            curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header("Content-Description: File Transfer");
+            header("Content-Transfer-Encoding: binary");
+            Header("Content-type: ". 'video/mp4');
+            //Header("Content-Length: ".$fileSize);
+            flush();
+
+            $result = curl_exec($curl);
+            curl_close($curl);
+
+        }
     }
 
     /**
