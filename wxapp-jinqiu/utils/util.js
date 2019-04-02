cd37ee91af31ca5d7e15fb59ef2a7836a4e337f3
zhhuang <z.huang@canior.com>
2019-04-02 22:29:54 +0800

    wxapp: bianxian merge to jinqiu, fix bug

--- wxapp-jinqiu/utils/util.js
+++ wxapp-jinqiu/utils/util.js
@@ -15,7 +15,11 @@ const formatNumber = n => {
 }
 
 const getQueryVariable = (url, variable) => {
-  var vars = url.split("&");
+  var urlStr = url;
+  if (url.lastIndexOf("?") > 0) {
+    urlStr = url.substring(url.lastIndexOf("?") + 1)
+  }
+  var vars = urlStr.split("&");
   for (var i = 0; i < vars.length; i++) {
     var pair = vars[i].split("=");
     if (pair[0] == variable) { return pair[1]; }
