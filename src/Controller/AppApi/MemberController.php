c17c554f8073a3370b76886dc47f87f2ac0f6b90
Jeff <vipmadu@qq.com>
2019-05-29 11:59:30 +0800

    修改密码

--- src/Controller/AppApi/MemberController.php
+++ src/Controller/AppApi/MemberController.php
@@ -78,8 +78,68 @@ class MemberController extends AbstractController
             return CommonUtil::resultData( [], ErrorCode::ERROR_LOGIN_PHONE_OR_CODE_ERROR )->toJsonResponse();
         }
 
-        // 重设密码
+        // 重设手机号
         $user->setPhone($data['phone']);
+        try {
+            $userManager->updateUser($user, true);
+        } catch (\Exception $e) {
+            return new JsonResponse(["error" => $e->getMessage()], 500);
+        }
+
+        // 返回
+        return CommonUtil::resultData([])->toJsonResponse();
+    }
+
+    /**
+     * @Route("/setNewPwd", name="apiSetNewPwd",  methods={"POST"})
+     * @param Request $request
+     * @param UserRepository $userRepository
+     * @param EncoderFactoryInterface $encoderFactory
+     * @param JWTTokenManagerInterface $JWTTokenManager
+     * @param MessageCodeRepository $messageCodeRepository
+     * @return JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     * @param UserManagerInterface $userManager
+     */
+    public function setNewPwd(
+        Request $request, 
+        UserRepository $userRepository,
+        EncoderFactoryInterface $encoderFactory,
+        JWTTokenManagerInterface $JWTTokenManager,
+        MessageCodeRepository $messageCodeRepository,
+        UserManagerInterface $userManager,
+        TokenStorageInterface $tokenStorage
+    )
+    {
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+
+        // 请求参数验证
+        $validator = Validation::createValidator();
+        $constraint = new Assert\Collection([
+            'oldPassword' => new Assert\Length(['min' => 6, 'max' => 30]),
+            'newPassword' => new Assert\Length(['min' => 6, 'max' => 30]),
+        ]);
+        $violations = $validator->validate($data, $constraint);
+        if ($violations->count() > 0) {
+            return CommonUtil::resultData([], ErrorCode::ERROR_LOGIN_USER_NOT_FIND, 417, (string)$violations)->toJsonResponse();
+        }
+
+        // 查询匹配用户
+        $user = $tokenStorage->getToken()->getUser();
+        if ($user == null) {
+            return CommonUtil::resultData( [], ErrorCode::ERROR_LOGIN_USER_NOT_FIND )->toJsonResponse();
+        }
+
+        // 验证旧密码
+        $passwordValid = $encoderFactory->getEncoder($user)->isPasswordValid($user->getPassword(),$data['oldPassword'], $user->getSalt());
+        if( !$passwordValid ){
+            return CommonUtil::resultData( [], ErrorCode::ERROR_LOGIN_USERNAME_OR_PASSWORD_ERROR )->toJsonResponse();
+        }
+
+        // 重设密码
+        $user->setPlainPassword($data['newPassword']);
         try {
             $userManager->updateUser($user, true);
         } catch (\Exception $e) {
