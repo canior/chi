9de427773d9b311d81b942bc89b7145b0f203304
zxqc2014 <zxqc2014@gmail.com>
2019-06-27 22:09:13 +0800

    跨域问题

---
+++ src/EventListener/ResponseListener.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zxqc2018
+ * Date: 2019/6/27
+ * Time: 21:41
+ */
+
+namespace App\EventListener;
+
+
+use App\Service\Util\CommonUtil;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class ResponseListener
+{
+    /**
+     * Filters the Response.
+     * @param FilterResponseEvent $event
+     */
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $response = $event->getResponse();
+        if (!empty($event->getRequest()->getRequestUri()) && CommonUtil::requestUrlStartsWith($event->getRequest(), 'appApi||gongZhong')) {
+            $origin = $event->getRequest()->server->get('HTTP_ORIGIN');
+            $allowOrigin = [
+                'https://gongzhong.zscollege.com.cn',
+            ];
+
+            if(CommonUtil::isDebug()){
+                $allowOrigin[] = 'http://localhost:8080';
+            }
+
+            if (in_array($origin, $allowOrigin)) {
+                $response->headers->set('Access-Control-Allow-Origin', $origin);
+                $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
+                $response->headers->set('Access-Control-Expose-Headers', 'Authorization, authenticated');
+                $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT','OPTIONS');
+                $response->headers->set('Access-Control-Max-Age', 18000);
+                $response->headers->set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+                $response->headers->set('Access-Control-Allow-Credentials', 'true');
+            }
+        }
+
+        $response->prepare($event->getRequest());
+    }
+}
