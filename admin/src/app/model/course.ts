9ddcb2f610328d16b83bfbf7e4e35ebd589b6b16
Jeff <vipmadu@qq.com>
2019-08-23 09:46:47 +0800

    page

--- admin/src/app/model/course.ts
+++ admin/src/app/model/course.ts
@@ -2,20 +2,28 @@
 export class Course {
 	public id:string;
 	public title:string;
-	public content:string;
-	public remark:string;
-	public image:string;
-	public from:string;
-	public category_name:string;
-	public author:string;
+
+	public category_id:any;
 	public category:string;
+	public category_title:string;
+	public start_date:string;
+	public end_date:string;
+	public category_name:string;
+	public teacher_id:string;
+	public teacher_name:string;
 	public top:string;
 	public teacher:any;
-	public image_url:any;
-	public aliyunVideoId:any;
-	// public aliyunVideoId:any;
+	public image:any;
+	public remark:any;
+	public content:any;
+	public video_key:any;
+	public status:any;
 	public unlockType:any;
-	// public aliyunVideoId:any;
-	// public aliyunVideoId:any;
+	public album_title:any;
+	public album_id:any;
+	
 	public link:any;
+	public course_tag:any;
+	public image_url:any;
+	public unlock_type:any; 
 }
