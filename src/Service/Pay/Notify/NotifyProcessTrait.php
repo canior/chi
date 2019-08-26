50f62a94f6d8c970895b1a66312f0c7e62a834c6
Jeff <vipmadu@qq.com>
2019-08-26 09:25:51 +0800

    fix

--- src/Service/Pay/Notify/NotifyProcessTrait.php
+++ src/Service/Pay/Notify/NotifyProcessTrait.php
@@ -68,23 +68,24 @@ trait NotifyProcessTrait
         if ($product->isCourseProduct() && !$product->getCourse()->isOnline()) {
             $course = $product->getCourse();
             if ($course->isSystemSubject()) {
+
+                //有上一级则发送消息
+                if (!empty($groupUserOrder->getUser()->getBianxianTopParentPartnerUpUser())) {
+                    $message = new MessageGroupUserOrderMeta();
+                    $name = CommonUtil::getInsideValue($groupUserOrder, 'getUser.getName', '');
+                    $phone = CommonUtil::getInsideValue($groupUserOrder, 'getUser.getPhone', '');
+                    $message->setDataId($groupUserOrder->getId());
+                    $message->setUser($groupUserOrder->getUser()->getBianxianTopParentPartnerUpUser());
+                    $message->setContent("您的用户{$name},手机号为{$phone}完成了系统学员的身份升级，您的名额将扣除一个。");
+                    $message->setTitle("学员升级");
+                    CommonUtil::entityPersist($message);
+                }
+
                 if ($user->isSystemSubjectPrivilege(false)) {
                     $groupUserOrder->setTableNo((int)OfflineTableNo::getUserTable($groupUserOrder));
                     $groupUserOrder->setCheckStatus(GroupUserOrder::CHECK_PASS);
                     $groupUserOrder->setCheckAt(time());
 
-                    //有上一级则发送消息
-                    if (!empty($groupUserOrder->getUser()->getBianxianTopParentPartnerUpUser())) {
-                        $message = new MessageGroupUserOrderMeta();
-                        $name = CommonUtil::getInsideValue($groupUserOrder, 'getUser.getName', '');
-                        $phone = CommonUtil::getInsideValue($groupUserOrder, 'getUser.getPhone', '');
-                        $message->setDataId($groupUserOrder->getId());
-                        $message->setUser($groupUserOrder->getUser()->getBianxianTopParentPartnerUpUser());
-                        $message->setContent("您的用户{$name},手机号为{$phone}完成了系统学员的身份升级，您的名额将扣除一个。");
-                        $message->setTitle("学员升级");
-                        CommonUtil::entityPersist($message);
-                    }
-                
                     //todo sms通知
                     $data['nextPageType'] = 4;
                 } else {
