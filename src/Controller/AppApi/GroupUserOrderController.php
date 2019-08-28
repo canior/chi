33d1ae4ee63640535174b8ad8cf8986c7971532c
Jeff <vipmadu@qq.com>
2019-08-28 11:30:51 +0800

    appGroupUserOrdersAction

--- src/Controller/AppApi/GroupUserOrderController.php
+++ src/Controller/AppApi/GroupUserOrderController.php
@@ -20,6 +20,7 @@ use App\Service\Util\CommonUtil;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 
 /**
  * Class GroupUserOrderController
@@ -194,5 +195,102 @@ class GroupUserOrderController extends AppApiBaseController
         return $requestProcess->toJsonResponse([
             'groupUserOrder' => $offlineCourseOrder->getArray()
         ]);
+    }
+
+    /**
+     * 我的订单列表
+     *
+     * 全部，待成团， 待发货， 已发货， 待收货
+     *
+     * 全部: status = null, paymentStatus in ['paid', 'refunding', 'refunded']
+     * 待成团: status = 'created', paymentStatus = 'paid'
+     * 待发货: status = 'pending', paymentStatus = 'paid'
+     * 已发货：status = 'shipping' paymentStatus = 'paid'
+     * 已收货: status = 'delivered' paymentStatus = 'paid'
+     *
+     * @Route("/groupUserOrders", name="appApiGroupUserOrders", methods="POST")
+     * @param Request $request
+     * @param GroupUserOrderRepository $groupUserOrderRepository
+     * @return Response
+     */
+    public function appGroupUserOrdersAction(Request $request, GroupUserOrderRepository $groupUserOrderRepository, UserRepository $userRepository) {
+
+        $data = $this->processRequest($request, ['productType', 'page','recommander'], ['productType']);
+
+        $groupUserOrderStatus = isset($data['groupUserOrderStatus']) ? $data['groupUserOrderStatus'] : null;
+        $page = isset($data['page']) ? $data['page'] : 1;
+
+        /**
+         * product, onlineCourse, offlineCourse
+         */
+        $productType = isset($data['productType']) ? $data['productType'] : '';
+        $productCategory = isset($data['productCategory']) ? $data['productCategory'] :'';
+        $recommander = isset($data['recommander']) ? $data['recommander'] :false;
+
+        // 查询匹配用户
+        $user =  $this->getAppUser();
+        if ($user == null) {
+            return CommonUtil::resultData( [], ErrorCode::ERROR_LOGIN_USER_NOT_FIND )->toJsonResponse();
+        }
+
+        if ($groupUserOrderStatus == null){
+            $groupUserOrderStatus =  array_keys(GroupUserOrder::$statuses);
+        }
+        $paymentStatusArray = ['paid', 'refunding', 'refunded'];
+
+        switch ($productType) {
+            case 'product':
+                $where = [
+                    'status' => $groupUserOrderStatus,
+                    'paymentStatus' => $paymentStatusArray,
+                    'isCourseProduct'=>false,
+                ];
+                break;
+            case 'onlineCourse':
+                $where = [
+                    'status' => $groupUserOrderStatus,
+                    'paymentStatus' => $paymentStatusArray,
+                    'isCourseProduct'=>true,
+                    'isOnline'=>true
+                ];
+                break;
+            case 'offlineCourse':
+                $where = [
+                    'status' => $groupUserOrderStatus,
+                    'paymentStatus' => $paymentStatusArray,
+                    'isCourseProduct'=>true,
+                    'isOnline'=>false
+                ];
+                break;
+            default:
+                break;
+        }
+
+        // 推荐人订单，自己订单
+        if($recommander){
+            $recommandersUser = $userRepository->getUserByParent($user->getId());
+            $recommanders = [];
+            foreach ($recommandersUser as $k => $v) {
+                $recommanders[] = $v->getId();
+            }
+            $where['recommanders'] = $recommanders;
+        }else{
+            $where['userId'] = $user->getId();
+        }
+
+        $groupUserOrders = $groupUserOrderRepository->findUserGroupUserOrders($where);
+        $groupUserOrders = $this->getPaginator()->paginate($groupUserOrders, $page,self::PAGE_LIMIT);
+        $groupUserOrdersCount = $groupUserOrderRepository->findUserGroupUserOrders($where,true);
+
+        $groupUserOrdersArray = [];
+        foreach ($groupUserOrders as $groupUserOrder) {
+            $groupUserOrdersArray[] = $groupUserOrder->getArray();
+        }
+
+        // 返回
+        return CommonUtil::resultData( [
+            'groupUserOrders' => $groupUserOrdersArray,
+            'total' => CommonUtil::getTotalQueryCount($groupUserOrdersCount),
+        ] )->toJsonResponse();
     }
 }
